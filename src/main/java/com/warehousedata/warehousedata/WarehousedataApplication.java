8bd3428df47e79d82a9fabbc6390facd38e0855a
mouadHILLALI <mauadtoto001@gmail.com>
2025-11-10 16:25:38 +0100

    initial commit

---
+++ src/main/java/com/warehousedata/warehousedata/WarehousedataApplication.java
@@ -0,0 +1,13 @@
+package com.warehousedata.warehousedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehousedataApplication.class, args);
+	}
+
+}
